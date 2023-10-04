fb78b2f8e076734d6f6d63930e825c88b3348906
9984398@gmail.com <9984398@gmail.com>
2023-10-04 14:42:07 +0000

    Edited FormWeather

--- client_code/FormWeather/__init__.py
+++ client_code/FormWeather/__init__.py
@@ -13,3 +13,13 @@ class FormWeather(FormWeatherTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def text_box_1_focus(self, **event_args):
+    """This method is called when the TextBox gets focus"""
+    pass
+
+
