49b354452abdb209c5060b7b484d09c127af5f1d
9984398@gmail.com <9984398@gmail.com>
2023-10-05 16:32:20 +0000

    Edited FormWeather

--- client_code/FormWeather/__init__.py
+++ client_code/FormWeather/__init__.py
@@ -6,18 +6,19 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import requsts as r
+import anvil.http
 
 class FormWeather(FormWeatherTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.weather_url = ''
 
     # Any code you write here will run before the form opens.
 
   def text_box_1_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
     print(f'Received new location : {self.text_box_1.text}')
+    # weather_response = 
 
   def text_box_1_focus(self, **event_args):
     """This method is called when the TextBox gets focus"""
