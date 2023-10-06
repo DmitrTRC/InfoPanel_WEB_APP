4add0b0b2ccc901a2ad7b8a1792e251f01fdd27d
9984398@gmail.com <9984398@gmail.com>
2023-10-06 21:17:11 +0000

    Edited FormWeather

--- client_code/FormWeather/__init__.py
+++ client_code/FormWeather/__init__.py
@@ -12,7 +12,7 @@ class FormWeather(FormWeatherTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self._weather_url = 'https://v2d.wttr.in/wttr.in/'
+    self._weather_url = 'https://v2d.wttr.in/'
     self._weather_format = '?2AF'
 
     # Any code you write here will run before the form opens.
@@ -23,7 +23,7 @@ class FormWeather(FormWeatherTemplate):
     return weather_response.get_bytes()
 
   def text_box_1_pressed_enter(self, **event_args):
-    info = self.get_weather().decode('utf-8')
+    info = self.get_weather().decode()
     print (f'Raw response: {info}')
     self.rich_text_weather.content = info
 
