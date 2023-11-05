ca6a5cd20eac1e6a1830bbfdf5df3724b83b6d15
Dmitry Morozov <9984398@gmail.com>
2023-11-06 00:14:18 +0300

    --> Weather report: fix

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,18 +9,16 @@ import anvil.users
 
 from ..FormWeather import FormWeather
 
+
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+        # Any code you write here will run before the form opens.
 
-  def link_weather_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    weather_panel = FormWeather()
-    get_open_form().content_panel.clear()
-    get_open_form().content_panel.add_component(weather_panel)
-    
-    
-
+    def link_weather_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        weather_panel = FormWeather()
+        get_open_form().content_panel.clear()
+        get_open_form().content_panel.add_component(weather_panel)
