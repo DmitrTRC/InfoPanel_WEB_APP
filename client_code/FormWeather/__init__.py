495e155782221d3e9b3dca43275223b95e6780b6
9984398@gmail.com <9984398@gmail.com>
2023-11-27 18:11:46 +0000

    Refactoring server code

--- client_code/FormWeather/__init__.py
+++ client_code/FormWeather/__init__.py
@@ -1,5 +1,4 @@
 import anvil.google.auth, anvil.google.drive
-import anvil.http as rq
 import anvil.tables as tables
 import anvil.tables.query as q
 import anvil.users
@@ -13,22 +12,19 @@ from anvil.tables import app_tables
 from utils import report
 
 
+
 class FormWeather(FormWeatherTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        self._weather_url = 'https://wttr.in/'
-        self._weather_format = '?format=j1'  # 2AF
+        
 
         # Any code you write here will run before the form opens.
 
-    def get_weather(self):
-        weather_request = self._weather_url + self.text_box_1.text + self._weather_format
-        weather_response = rq.request(weather_request, json=True)
-        return weather_response
+  
 
     def text_box_1_pressed_enter(self, **event_args):
-        info_json = self.get_weather()
+        info_json = anvil.server.call('get_weather', self.text_box_1.text)
         print(f'Raw response: {info_json}')
 
         self.rich_text_weather.content = report.format_weather_report(info_json)
---
+++ server_code/weather.py
@@ -0,0 +1,31 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.http as rq
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def get_weather(position):
+  _weather_url = 'https://wttr.in/'
+  _weather_format = '?format=j1'  # 2AF
+  
+  weather_request = _weather_url + position + _weather_format
+  weather_response = rq.request(weather_request, json=True)
+  return weather_response
+  
